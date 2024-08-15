f05d91203f36dbad580cd3b1cea1ebeb2969cfc8
Geetam <geetambinani6@gmail.com>
2024-08-15 11:25:07 +0530

    remove unwanted imports

--- src/api/MyRestaurantApi.tsx
+++ src/api/MyRestaurantApi.tsx
@@ -1,6 +1,6 @@
 import { Restaurant } from "@/types/types";
 import { useAuth0 } from "@auth0/auth0-react";
-import { QueryClient, useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { toast } from "sonner";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
